3b329936f6a2f13f5612c9fca4a2e498a08f00da
Nikhil <nikhil.mulik@icruxsystem.com>
2022-07-11 20:08:42 +0530

    Num5 = 55

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,8 +3,9 @@ public class AddTwoNumbers
 
    public static void main(String[] args) 
 	{
-        int num3 = 33;
-	int num4 = 4;
+      int num3 = 33;
+	   int num4 = 4;
+      int num5 = 55;
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
 
